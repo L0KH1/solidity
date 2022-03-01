1e0677c0b7d446399615d5da7b0a433f383e0998
allil22@mail.wlu.edu <43547464+L0KH1@users.noreply.github.com>
2022-03-01 11:38:37 +0000

    Modified waver

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -5,7 +5,25 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    address[] waveArray;
+
     constructor() {
-        console.log("Yo, a proper smart contract - excellent.");
+        console.log("Yo, I'm a proper smart contract. Excellent.");
+    }
+    function wave() public {
+        address sender = payable(msg.sender);
+        waveArray.push(sender);
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        uint256 totalWaves = waveArray.length;
+        console.log("We have %d total waves!", totalWaves);
+        if (totalWaves != 0){
+            for (uint256 i =0; i< totalWaves; i++){
+                console.log("Waver %d", i, ": ", waveArray[i]);
+            }
+        }
+        return totalWaves;
     }
 }
