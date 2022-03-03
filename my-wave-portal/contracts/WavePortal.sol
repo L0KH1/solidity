adb56be407383041f69852ffde545d616e46e943
allil22@mail.wlu.edu <43547464+L0KH1@users.noreply.github.com>
2022-03-03 08:55:47 +0000

    today's work

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -5,25 +5,42 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
-    address[] waveArray;
+
+    uint256 totalWaves;
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
+    }
+
+    Wave[] waves;
 
     constructor() {
         console.log("Yo, I'm a proper smart contract. Excellent.");
     }
-    function wave() public {
-        address sender = payable(msg.sender);
-        waveArray.push(sender);
-        console.log("%s has waved!", msg.sender);
+    
+    function wave(string memory _message) public {
+        totalWaves += 1;
+        console.log("%s waved w/ message: %s", msg.sender, _message);
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
-        uint256 totalWaves = waveArray.length;
+        // Optional: Add this line if you want to see the contract print the value!
+        // We'll also print it over in run.js as well.
         console.log("We have %d total waves!", totalWaves);
-        if (totalWaves != 0){
-            for (uint256 i =0; i< totalWaves; i++){
-                console.log("Waver %d", i, ": ", waveArray[i]);
-            }
-        }
         return totalWaves;
     }
 }
+
+
+// wave portal address: 0xFb9eC1025C22865d49BA9528B0201bC194383a47
